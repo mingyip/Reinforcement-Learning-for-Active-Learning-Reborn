966d372b918d78cb35be11bdda59d9c5142de8ab
mingyip <fypcheung@gmail.com>
2019-06-20 17:25:10 +0200

    Implemented train_start and train end
    This is the first RL agent who can achieve same performance as Best vs Second best agent.

--- python/learningai/agent/valueAgent.py
+++ python/learningai/agent/valueAgent.py
@@ -30,6 +30,8 @@ class valueAgent(object):
         epochs                  = Config.CLASSIFICATION_EPOCH
         selection_size          = Config.SELECTION_BATCHSIZE
         train_size              = Config.TRAINING_BATCHSIZE
+        train_start_at          = Config.TRAINING_START_AT_ITER
+        train_end_at            = Config.TRAINING_FINISH_AT_ITER
         exporation_rate         = 1.0
         exporation_decay_rate   = Config.EXPLORATION_DECAY_RATE
         validation_images       = 1500
@@ -42,20 +44,27 @@ class valueAgent(object):
         last_remain_episodes    = None
         last_status             = np.zeros((selection_size, self.num_class+2))
 
+
         for episode in range(episodes):
             self.begin_episode()
             reward = self.get_validation_accuracy(nImages=validation_images)
 
             for iteration in range(int(budget/train_size)):
+    
+                if iteration <  train_start_at: continue
+                if iteration >= train_end_at:   break
+
                 ntrained        = iteration * train_size
                 remain_budget   = (budget - ntrained) / budget
                 remain_new_bgt  = max(budget-ntrained-1, 0) / budget
                 remain_episodes = (episodes - episode) / episodes
                 S_old           = copy.deepcopy(S)
+                # reward_old      = reward
 
                 if (np.random.rand(1)[0]>exporation_rate):
                     # Predict Rewards of different actions
                     [x_select, y_select, select_idx] = self.env.get_next_selection_batch()
+
                     S[:, 0:-2] = self.get_next_state_from_env(x_select)
                     S[:, -2] = remain_budget
                     S[:, -1] = remain_episodes
@@ -79,13 +88,10 @@ class valueAgent(object):
                     pair = {"S":S_old, "select_idx":select_idx, "train_idx":train_idx, "S_":S, "R":reward}
                     self.memory.append(pair)
 
-
                 # Train Classification Network
                 self.train_env(batch_x, batch_y, epochs)
 
                 # Train DQN Network
-
-                
                 [x_new_select, y_new_select, select_idx] = self.env.get_next_selection_batch(peek=True)
                 S_new[:, 0:-2] = self.get_next_state_from_env(x_new_select)
                 S_new[:, -2] = remain_new_bgt
@@ -95,19 +101,30 @@ class valueAgent(object):
                 avg_V = np.mean(predicts_new[tops_new])
                 reward = self.get_validation_accuracy(nImages=validation_images)
 
+
                 self.train_agent(reward, S[train_idx], avg_V)
+
+                print("Eps:", episode, " Iter:", iteration, " Reward:", reward, end="\r")
                 # print("episode:", episode, "  iteration:", iteration, "reward: ", reward, "exporation_rate:", exporation_rate)
 
-            [top_reward, top_dist] = self.evaluate(isStream=True, trainTop=True)
-            [low_reward, low_dist] = self.evaluate(isStream=True, trainTop=False)
-            # print("Eps:", episode, " R:", reward, " ExpRate:", exporation_rate)
-            print(str.format('Eps:{0:3.0f} R:{1:.2f} ExpRt:{2:.2f} ', episode, top_reward, exporation_rate), end='')
+            [top_reward, top_dist, top_size] = self.evaluate(isStream=True, trainTop=True)
+            print(str.format('Eps:{0:3.0f} R:{1:.4f} S:{3} Exp:{2:.2f} ', episode, top_reward, exporation_rate, top_size), end='')
             print(str.format('dist:{0:3.0f} {1:3.0f} {2:3.0f} {3:3.0f} {4:3.0f} {5:3.0f} {6:3.0f} {7:3.0f} {8:3.0f} {9:3.0f}', top_dist[0], top_dist[1], top_dist[2], top_dist[3], top_dist[4], top_dist[5], top_dist[6], top_dist[7], top_dist[8], top_dist[9]))
-            print(str.format('        R:{0:.2f}            ', low_reward), end='')
-            print(str.format('dist:{0:3.0f} {1:3.0f} {2:3.0f} {3:3.0f} {4:3.0f} {5:3.0f} {6:3.0f} {7:3.0f} {8:3.0f} {9:3.0f}', low_dist[0], low_dist[1], low_dist[2], low_dist[3], low_dist[4], low_dist[5], low_dist[6], low_dist[7], low_dist[8], low_dist[9]))
+
+            # if top_reward > 0.95:
+            #     for i in range(10):
+            #         [top_reward, top_dist, top_size] = self.evaluate(isStream=True, trainTop=True)
+            #         # [low_reward, low_dist, low_size] = self.evaluate(isStream=True, trainTop=False)
+
+            #         # print("Eps:", episode, " R:", reward, " ExpRate:", exporation_rate)
+            #         print(str.format('        R:{0:.4f} S:{1}          ', top_reward, top_size), end='')
+            #         print(str.format('dist:{0:3.0f} {1:3.0f} {2:3.0f} {3:3.0f} {4:3.0f} {5:3.0f} {6:3.0f} {7:3.0f} {8:3.0f} {9:3.0f}', top_dist[0], top_dist[1], top_dist[2], top_dist[3], top_dist[4], top_dist[5], top_dist[6], top_dist[7], top_dist[8], top_dist[9]))
+            #         # print(str.format('        R:{0:.2f} S:{1}          ', low_reward, low_size), end='')
+            #         # print(str.format('dist:{0:3.`0f} {1:3.0f} {2:3.0f} {3:3.0f} {4:3.0f} {5:3.0f} {6:3.0f} {7:3.0f} {8:3.0f} {9:3.0f}', low_dist[0], low_dist[1], low_dist[2], low_dist[3], low_dist[4], low_dist[5], low_dist[6], low_dist[7], low_dist[8], low_dist[9]))
 
             if exporation_rate > 0:
                 exporation_rate -= exporation_decay_rate
+
 
     def begin_game(self):
         """ Reset the agent in the game """
@@ -150,10 +167,13 @@ class valueAgent(object):
         """ Train classification network with dataset """
         self.env.train_env(x_train, y_train, epoch)
 
-    def predict(self, state):
+    def predict(self, state, batchsize=None):
         """ Agent predicts the state-action value (accuracy) """
         # TODO: function predict write log directly to the result.log
         # TODO: Move the calculation steps to GPU
+
+        if batchsize is None:
+            batchsize = Config.TRAINING_BATCHSIZE
 
         # budget = np.full((Config.SELECTION_BATCHSIZE, 1), remainBudget, dtype=float)
         # episode = np.full((Config.SELECTION_BATCHSIZE, 1), remainEpisode, dtype=float)
@@ -161,8 +181,8 @@ class valueAgent(object):
         predict = (self.sess.run(self.dqn.V, feed_dict)).squeeze()
 
         ranked = np.argsort(predict)
-        low_idx = ranked[-Config.TRAINING_BATCHSIZE:]
-        top_idx = ranked[0:Config.TRAINING_BATCHSIZE]
+        low_idx = ranked[-batchsize:]
+        top_idx = ranked[0:batchsize]
 
         return predict, top_idx, low_idx
 
@@ -177,8 +197,9 @@ class valueAgent(object):
         iterations      = int(budget/train_size)
         S               = np.zeros((selection_size, self.num_class+2))
         remain_episodes = 0
-        validation_imgs = 1500
+        validation_imgs = -1
         distribution    = np.zeros((10))
+        trainSize       = 0
 
         self.reset_network()
         for iteration in range(iterations):
@@ -186,11 +207,11 @@ class valueAgent(object):
             ntrained        = iteration * train_size
             remain_budget   = (budget - ntrained) / budget
 
-            [x_select, y_select, idx] = self.env.get_next_selection_batch()
+            [x_select, y_select, idx] = self.env.get_next_selection_batch(batchsize=selection_size)
             S[:, 0:-2] = self.get_next_state_from_env(x_select)
             S[:, -2] = remain_budget
             S[:, -1] = remain_episodes
-            predicts, tops, lows = self.predict(S)
+            predicts, tops, lows = self.predict(S, batchsize=train_size)
             
             if trainTop:
                 train_idx = tops
@@ -199,11 +220,12 @@ class valueAgent(object):
 
             batch_x = x_select[train_idx]
             batch_y = y_select[train_idx]
+            trainSize = trainSize + len(train_idx)
             distribution = distribution + np.sum(batch_y, axis=0)
 
             self.train_env(batch_x, batch_y, epochs)
 
-        return [self.get_test_accuracy(validation_imgs), distribution]
+        return [self.get_test_accuracy(validation_imgs), distribution, trainSize]
 
     def storeNetworkVar(self):
         """ Store network variables so that later we can re-init the network """
